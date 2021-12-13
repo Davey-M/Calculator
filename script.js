2268894244fe6622abbe9f1df017abe284f821cf
Davey-M <davey.meuer@gmail.com>
2021-12-13 17:23:50 -0600

    added input controls for the calculator buttons

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
 const buttons = document.getElementsByClassName('button');
 const display = document.getElementById('display');
 
-const allowed_chars = ['1','2','3','4','5','6','7','8','9','0','-','+','x','*','/','.',];
+const allowed_chars = ['1','2','3','4','5','6','7','8','9','0','-','+','x','X','*','/','.','!','c','C','=','CE','^'];
+const numbers = ['1','2','3','4','5','6','7','8','9','0',];
+const multipliers = ['x','X','*'];
 
 function button_press(char)
 {
-  console.log(char);
+  if (!allowed_chars.includes(char))
+  {
+    return
+  }
+
+  if (multipliers.includes(char))
+  {
+    char = 'x';
+  }
+  
+  if (numbers.includes(char))
+  {
+    display.innerText += char
+  }
+  else if (char === 'c' || char === 'C')
+  {
+    display.innerText = display.innerText.substring(0, display.innerText.length - 1);
+  }
+  else if (char === 'CE')
+  {
+    display.innerText = '';
+  }
+  else if (char === '=')
+  {
+    let dis = display.innerHTML.toString();
+    let equation = dis.split('x').join('*').split('&nbsp;').join(' ').split(' ');
+    console.log(equation);
+  }
+  else
+  {
+    display.innerHTML += ' ' + char + '&nbsp';
+  }
 }
 
 for (let b of buttons)
